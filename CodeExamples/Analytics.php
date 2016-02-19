8043cda3d672e1c1855bd409650d1daf34679aec
George Miller <github@duffleman.co.uk>
2016-02-19 18:09:04 +0000

    Yay for Analytics (Code Examples)

---
+++ CodeExamples/Analytics.php
@@ -0,0 +1,16 @@
+<?php
+
+use Duffleman\Luno\LunoRequester;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$luno = new LunoRequester([
+    'key'     => '<YOUR_LUNO_KEY>',
+    'secret'  => '<YOUR_LUNO_SECRET>',
+    'timeout' => 10000
+]);
+
+// Cycle through the top 100 users.
+$user_analytics = $luno->analytics->users();
+
+echo("{$user_analytics['total']} users have signed up.");
